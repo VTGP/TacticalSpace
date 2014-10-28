3145ea665bb3fc90d48447bb36bcb015da52f752
unknown <danielkamick@gmail.com>
2014-10-27 21:36:51 -0400

    Creates the Grid Auto-generating system

---
+++ Assets/GridSystem.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridSystem : MonoBehaviour {
+	public GameObject plane;
+	public int width = 10;
+	public int length = 10;
+
+	private GameObject[,] grid = new GameObject[10,10];
+
+	void OnGUI()
+	{
+		if(GUI.Button (new Rect (10,10,150,100), "DEATH!"))
+		{
+			Destroy(grid [3, 3]);
+		}
+	}
+
+	void Start () {
+		for (int x=0; x < width; x++) 
+		{
+			for (int z=0; z < length; z++)
+			{
+				GameObject gridPlane = (GameObject)Instantiate(plane);
+				gridPlane.transform.position = new Vector3(gridPlane.transform.position.x + x, 
+					gridPlane.transform.position.y, gridPlane.transform.position.z + z);
+				grid[x,z] = gridPlane;
+			}
+		
+		}
+	}
+	
+	void Update () {
+	
+	}
+}
